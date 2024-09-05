9b144314603fdc0f77d267d5b922ed9de257102c
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-05 08:07:48 +0545

    Add routes for create and get food items

--- index.js
+++ index.js
@@ -11,3 +11,39 @@ app.use(express.json());
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+//array to store food items
+
+let foodItems = [];
+
+//route to handle create food item
+
+app.post('/food', (req, res) => {
+    const newItem = {
+        id: foodItems.length + 1,
+        name: req.body.name,
+        quantity: req.body.quantity
+    };
+    foodItems.push(newItem);
+    res.status(201).json(newItem);
+});
+
+
+//get all food items
+
+app.get('/food', (req, res) => {
+    res.json(foodItems);
+});
+
+//get specific food item
+app.get('/food/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const foodItem = foodItems.find(item => item.id === id);
+
+    if (foodItem) {
+        res.json(foodItem);
+    } else {
+        res.status(404).send('Food item not found');
+    }
+});
+
